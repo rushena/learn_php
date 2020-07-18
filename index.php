8cf6bacf785c75ac966a5d6c487b81a6e549c504
Rushena <rushena2706@gmail.com>
2020-07-19 03:45:01 +0500

    Добавлен файл index.php

---
+++ index.php
@@ -0,0 +1,54 @@
+<?php
+
+
+$connect = mysqli_connect('localhost', 'root', 'password', 'shop');
+
+if( mysqli_connect_errno() ) {
+
+	var_dump(mysqli_connect_error());
+
+	exit;
+}
+
+mysqli_set_charset($connect, 'utf8');
+
+$query = "SELECT * from `products`";
+
+$request = mysqli_query($connect, $query);
+
+if ( mysqli_errno($connect) ) {
+	var_dump(mysqli_error($connect));
+
+	exit;
+}
+
+$has_th = false;
+
+echo '<table border="1" celloadding="3">';
+
+while ($row = mysqli_fetch_assoc($request)) {
+
+	if (!$has_th) {
+		echo "<tr><th>#</th><th>Название товара</th><th></th></tr>";
+		$has_th = true;
+	}
+	echo "<tr>";
+
+		foreach($row as $fiels => $value) {
+			echo "<td>";
+			echo $value;
+			echo "</td>";
+		}
+
+		echo "<td>";
+		echo "<a href=\"edit.php\">Редактировать</a> | <a href=\"\">Удалить</a>";
+		echo "</td>";
+
+	echo "</tr>";
+
+
+	var_dump($row);
+} 
+
+
+echo '</table>';
